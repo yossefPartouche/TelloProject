4a8ab21c08aea6a40de6f36d6852030db282fa39
yossefpartouche <yossipartouche123@icloud.com>
2024-09-03 07:10:32 +0300

    simple intro to send_rc_control fucntion

---
+++ BasicMovements.py
@@ -0,0 +1,21 @@
+from djitellopy import tello
+from time import sleep
+
+d1 = tello.Tello()
+d1.connect()
+# Testing connection
+
+print(d1.get_battery())
+
+"""
+The logic sequence for testing myst always be:
+    1) take-off to get some height
+    2) apply flying tests
+    3) reduce directional speed to 0
+    4) land
+"""
+d1.takeoff()
+d1.send_rc_control(0,0,0,0)
+sleep(2)
+d1.send_rc_control(0,0,0,0)
+d1.land()
